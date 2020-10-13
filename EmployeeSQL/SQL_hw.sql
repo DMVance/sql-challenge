37b4402f3dc03f3568937bdefea7a8661e724584
DMVance <csljrrt@gmail.com>
2020-10-13 00:12:07 -0500

    add SELECT statement FROM employees with condition

--- EmployeeSQL/SQL_hw.sql
+++ EmployeeSQL/SQL_hw.sql
@@ -55,5 +55,8 @@ SELECT employees.emp_no, last_name, first_name, sex, salary
 	FROM employees
     inner join salaries on employees.emp_no = salaries.emp_no
 
+SELECT first_name, last_name, hire_date FROM employees
+	WHERE hire_date > '1986-01-01' AND hire_date < '1987-01-01'
+
 
 
