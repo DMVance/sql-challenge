a5ea92b0cf0b3e19ec6d95a0d6c1fa0671658163
DMVance <csljrrt@gmail.com>
2020-10-12 21:18:37 -0500

    add foreign key to dept_manager table

--- EmployeeSQL/SQL_hw.sql
+++ EmployeeSQL/SQL_hw.sql
@@ -35,3 +35,6 @@ CREATE TABLE "salaries" (
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
