a0ca06bfbec938d92379d4701c31a1409389871a
DMVance <csljrrt@gmail.com>
2020-10-13 15:08:49 -0500

    add query for name and sex using where clause with two conditions

--- EmployeeSQL/SQL_hw.sql
+++ EmployeeSQL/SQL_hw.sql
@@ -53,18 +53,22 @@ REFERENCES "employees" ("emp_no");
 
 SELECT employees.emp_no, last_name, first_name, sex, salary 
 	FROM employees
-    inner join salaries on employees.emp_no = salaries.emp_no
+    INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 SELECT first_name, last_name, hire_date FROM employees
-	WHERE hire_date > '1986-01-01' AND hire_date < '1987-01-01'
+	WHERE hire_date > '1986-01-01' AND hire_date < '1987-01-01';
 
-SELECT e.first_name, e.last_name, dp.dept_name, dp.dept_no, dp.dept_name, dm.emp_no FROM employees as e
-	inner join dept_manager as dm on dm.emp_no = e.emp_no
-	inner join departments as dp on dp.dept_no = dm.dept_no
+SELECT e.first_name, e.last_name, dp.dept_name, dp.dept_no, dp.dept_name, dm.emp_no FROM employees AS e
+	INNER JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
+	INNER JOIN departments AS dp ON dp.dept_no = dm.dept_no;
 
-SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name FROM employees as e
-	inner join dept_emp as de on de.emp_no = e.emp_no
-	inner join departments as dp on dp.dept_no = de.dept_no 
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name FROM employees AS e
+	INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+	INNER JOIN departments AS dp ON dp.dept_no = de.dept_no;
+
+SELECT e.first_name, e.last_name, e.sex FROM employees as e
+	WHERE e.first_name = 'Hercules' and
+	e.last_name LIKE 'B%';
 
 
 
